eba5befa0b67d1104f257d1269f8b7f5745127d3
celinee <cmdpayla22@src.pshs.edu.ph>
2025-04-30 07:57:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gameData = {
         {
             images: ["images/sun1.jpg", "images/sun2.jpg", "images/sun3.jpg", "images/sun4.jpg"],
             answer: "SUN",
-            hint: "Shines bright in the sky"
+            hint: "causes skin cancer"
         },
         {
             images: ["images/cute1.jpg", "images/cute2.jpg", "images/cute3.jpg", "images/cute4.jpg"],
@@ -18,12 +18,12 @@ const gameData = {
         {
             images: ["images/yogurt1.jpg", "images/yogurt2.jpg", "images/yogurt3.jpg", "images/yogurt4.jpg"],
             answer: "YOGURT",
-            hint: "Healthy dairy product"
+            hint: "me: peaceful  ful: where u going  u: i ain't going anywhere  gurt: yo"
         },
         {
             images: ["images/tree1.jpg", "images/tree2.jpg", "images/tree3.jpg", "images/tree4.jpg"],
             answer: "TREE",
-            hint: "Grows in the forest"
+            hint: "1 2 "
         }
     ],
     hard: [
@@ -35,22 +35,22 @@ const gameData = {
         {
             images: ["images/pisay1.jpg", "images/pisay2.jpg", "images/pisay3.jpg", "images/pisay4.jpg"],
             answer: "PISAY",
-            hint: "Philippine Science High School"
+            hint: "Philippine Science High Thou stands above with thy thoughts that lift."
         },
         {
             images: ["images/hacker1.jpg", "images/hacker2.jpg", "images/hacker3.jpg", "images/hacker4.jpg"],
             answer: "HACKER",
-            hint: "Computer security expert"
+            hint: "me after comsi"
         },
         {
             images: ["images/jovani1.jpg", "images/jovani2.jpg", "images/jovani3.jpg", "images/jovani4.jpg"],
             answer: "JOVANI",
-            hint: "A name that starts with J"
+            hint: "best teacher"
         },
         {
             images: ["images/kyleenorio1.jpg", "images/kyleenorio2.jpg", "images/kyleenorio3.jpg", "images/kyleenorio4.jpg"],
             answer: "KYLEENORIO",
-            hint: "A unique last name"
+            hint: "comsi hacker gcahs hacker"
         }
     ]
 };
